d817cbc2652e813c5a5bc53aac013f807df5a560
shutorm02 <shutorm2106@gmail.com>
2024-11-23 11:54:48 +0900

    UPDATE: 入力文字が新規の文字かつ回答に含まれる場合は回答を開放する、それ以外は制限回数を減らす

--- src/game_start.rb
+++ src/game_start.rb
@@ -3,10 +3,17 @@ require_relative './lib/word_game'
 word_game = WordGame.new
 
 while word_game.remaining_life > -1
-  input_chars = gets.chomp
+  input_char = gets.chomp
 
-  if !word_game.correct_chars?(input_chars)
+  if !word_game.correct_char?(input_char)
     puts '入力できるのはアルファベットの大文字または小文字の1文字のみです'
     next
+  end
+
+  if word_game.check_char_whether_exist_in_the_answer(input_char)
+    puts '正解です！'
+    break if word_game.correct_answer?
+  else
+    puts '残念！このアルファベットではないようです'
   end
 end
--- src/lib/word_game.rb
+++ src/lib/word_game.rb
@@ -3,13 +3,14 @@ class WordGame
   WORD_LIST = ['dog', 'cat', 'rabbit', 'tiger', 'raccoon', 'cow', 'bear', 'whale']
   DEFAULT_REMAINING_LIFE = 5
 
-  attr_accessor :remaining_life, :displayed_word
+  attr_accessor :remaining_life, :displayed_word, :input_chars
   attr_reader :answer_word
 
   def initialize()
     @answer_word = WORD_LIST.sample
     @displayed_word = create_initial_word(@answer_word.size)
     @remaining_life = DEFAULT_REMAINING_LIFE
+    @input_chars = []
 
     puts <<-EOT
 --------------------------------
@@ -22,8 +23,38 @@ class WordGame
     EOT
   end
 
-  def correct_chars?(chars)
-    !!chars.match(/[a-zA-Z]/) && chars.size == 1
+  def correct_char?(char)
+    !!char.match(/[a-zA-Z]/) && char.size == 1
+  end
+
+  def is_already_input_char?(char)
+    @input_chars.include?(char)
+  end
+
+  def not_exist_char_in_the_answer?(char)
+    !@answer_word.include?(char)
+  end
+
+  def open_answer_word
+    formatted_chars = @input_chars.map { |char| "^#{char}" }.join('|')
+    @displayed_word = @answer_word.tr(formatted_chars, "_")
+  end
+
+  def check_char_whether_exist_in_the_answer(char)
+    char = char.downcase
+
+    if not_exist_char_in_the_answer?(char) || is_already_input_char?(char)
+      @remaining_life -= 1
+      false
+    else
+      @input_chars.push(char)
+      open_answer_word
+      true
+    end
+  end
+
+  def correct_answer?
+    @displayed_word == @answer_word
   end
 
   private
